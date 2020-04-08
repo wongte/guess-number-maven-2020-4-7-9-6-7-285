8028fe297f133b7957c9a080b672bd057f38087e
Terrence Wong <wongte@oocl.com>
2020-04-08 19:21:42 +0800

    [Terrence] - Change getNumberOfCorrectNumber, getNumberOfCorrectPosition, generateResultOutput to private method

--- src/main/java/com/oocl/Game.java
+++ src/main/java/com/oocl/Game.java
@@ -1,14 +1,12 @@
 package com.oocl;
 
-import java.util.Arrays;
-
 public class Game {
     private int[] answer = new int[4];
     public void setAnswer(int[] answer) {
         this.answer = answer;
     }
 
-    public int getNumberOfCorrectNumber(int[] guess) {
+    private int getNumberOfCorrectNumber(int[] guess) {
         int numberOfCorrectNumber = 0;
         for (int eachGuess : guess) {
             for (int eachAnswer : this.answer) {
@@ -18,7 +16,7 @@ public class Game {
         return numberOfCorrectNumber;
     }
 
-    public int getNumberOfCorrectPosition(int[] guess) {
+    private int getNumberOfCorrectPosition(int[] guess) {
         int numberOfCorrectPosition = 0;
         for (int index = 0; index < answer.length; index++) {
             numberOfCorrectPosition += answer[index] == guess[index] ? 1 : 0;
@@ -26,7 +24,7 @@ public class Game {
         return numberOfCorrectPosition;
     }
 
-    public String generateResultOutput(int numberOfCorrectNumber, int numberOfCorrectPosition) {
+    private String generateResultOutput(int numberOfCorrectNumber, int numberOfCorrectPosition) {
         int b = numberOfCorrectNumber - numberOfCorrectPosition;
         return String.format("%dA%dB", numberOfCorrectPosition, b);
     }
--- src/test/java/com/oocl/GameTest.java
+++ src/test/java/com/oocl/GameTest.java
@@ -5,62 +5,6 @@ import org.junit.Test;
 
 public class GameTest {
     @Test
-    public void test_get_number_of_correct_number_with_0_correct() {
-        Game game = new Game();
-        int[] answer = {1, 2, 3, 4};
-        int[] guess = {6, 7, 8, 9};
-        int expectedResult = 0;
-        game.setAnswer(answer);
-
-        int result = game.getNumberOfCorrectNumber(guess);
-        Assert.assertEquals(expectedResult, result);
-    }
-
-    @Test
-    public void test_get_number_of_correct_number_with_2_correct() {
-        Game game = new Game();
-        int[] answer = {1, 2, 3, 4};
-        int[] guess = {6 , 7, 2, 1};
-        int expectedResult = 2;
-        game.setAnswer(answer);
-
-        int result = game.getNumberOfCorrectNumber(guess);
-        Assert.assertEquals(expectedResult, result);
-    }
-
-    @Test
-    public void test_get_number_of_correct_position_with_0_correct() {
-        Game game = new Game();
-        int[] answer = {1, 2, 3, 4};
-        int[] guess = {4, 3, 2, 1};
-        int expectedResult = 0;
-        game.setAnswer(answer);
-
-        int result = game.getNumberOfCorrectPosition(guess);
-        Assert.assertEquals(expectedResult, result);
-    }
-
-    @Test
-    public void test_get_number_of_correct_position_with_2_correct() {
-        Game game = new Game();
-        int[] answer = {1, 2, 3, 4};
-        int[] guess = {1, 2, 5, 6};
-        int expectedResult = 2;
-        game.setAnswer(answer);
-
-        int result = game.getNumberOfCorrectPosition(guess);
-        Assert.assertEquals(expectedResult, result);
-    }
-
-    @Test
-    public void test_generate_result_output() {
-        Game game = new Game();
-        String expectedResult = "1A2B";
-        String actualResult = game.generateResultOutput(3, 1);
-        Assert.assertEquals(expectedResult, actualResult);
-    }
-
-    @Test
     public void test_check_result_with_0a0b() {
         Game game = new Game();
         int[] answer = {1, 2, 3, 4};
