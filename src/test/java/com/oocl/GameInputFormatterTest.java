cb557a4565c9e4dccb3a40262cb561e41ff5caed
Terrence Wong <wongte@oocl.com>
2020-04-13 21:37:31 +0800

    [Terrence] - rename GameInputValidatorTest to GameInputFormatterTest

--- src/test/java/com/oocl/GameInputFormatterTest.java
+++ src/test/java/com/oocl/GameInputFormatterTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class GameInputValidatorTest {
+public class GameInputFormatterTest {
     private GameInputFormatter gameInputValidator;
 
     @Before
